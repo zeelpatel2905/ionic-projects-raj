ed2bed2d6b9efd89ba072f26db912947d6903ce8
RajZalavadiya <92564903+raj4920@users.noreply.github.com>
2022-09-23 12:46:50 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    Geolocation,
-  ],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
